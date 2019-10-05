ead851ce3cb0e3be9345e2e55e6b0894ea299532
Ramiro Gisone <ramirogisone@gmail.com>
2019-10-05 20:36:17 -0300

    actualizo clase-2 JS

---
+++ form-1.js
@@ -0,0 +1,8 @@
+const submitForm = (ev) => {
+    ev.preventDefault();
+    console.log('Va el submit', ev.target);
+}
+
+const addMember = () => {
+    const familyContainer = document.getElementById('family-info');
+}
